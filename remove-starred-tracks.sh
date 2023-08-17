11aadc774b2705076643fc7349e57550252e6aac
doctor-nam <potatochip81@gmail.com>
2023-08-17 12:36:28 +0900

    first commit

--- remove-starred-tracks.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sqlite3 /data/data/com.termux/files/home/navidrome/data/navidrome.db "select path from annotation a, media_file f where a.item_id = f.id AND (a.starred OR a.rating = 1);" | xargs -d '\n' rm
